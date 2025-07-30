a8d0df5b38e70ddf149eeb6785f3254c4f67dea1
yashwanth369s <palvayiyashwanthsai@gmail.com>
2025-07-30 18:36:00 +0530

    Add files via upload

---
+++ gitcode2.c
@@ -0,0 +1,13 @@
+//This code is to print a string without using a semicolon
+#include<stdio.h>
+void main(void)
+{
+/*here printf returns the no of characters in the input
+  then excutes printf then empty block without using semicolon
+  */ 
+    if(printf("product based MNC"))
+    {}  
+
+    if(printf("service based MNC"))
+    {}
+}
